2cdbfc9a60eeffda0d07a13d74aa57c709271dfe
Brandon Zuk <bzuk@premierlogic.com>
2015-08-17 16:14:41 +0000

    Changes made by Brandon Zuk (bzuk@premierlogic.com).

--- Checknet Application/js/checknet.angular.js
+++ Checknet Application/js/checknet.angular.js
@@ -20,14 +20,31 @@
 /*var app = new kendo.mobile.Application($(document.body), { skin: 'nova' });*/
 
 angular.module('checkApp', ['kendo.directives'])
-    .controller("MyCtrl", function ($scope) {
-        $scope.panelBarOptions = {
-            contentUrls: [null, null, "../content/web/loremIpsum.html"]
-        };
-        $scope.hello = "Hello from controller";
+    .controller('test', function($scope) {
+        $scope.focusTextbox = function() {
+          console.log(1);
+            $("#input").focus().select();
+         
+        }
     })
 
-function focusTextbox() {
-    $("#input").focus();
-}
+angular.module('directivesModule').directive('focusOnClear', [
+    '$timeout',
+    function (timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var id = attrs.focusOnClear;
+                var $inputSearchElement = $(element).parent().find('#' + id);
+                element.on('click', function (event) {
+                    event.preventDefault();
+                    timeout(function () {
+                        $inputSearchElement.focus();
+                    }, 500);
+                });
+            }
+        };
+    }
+]);
+
 
