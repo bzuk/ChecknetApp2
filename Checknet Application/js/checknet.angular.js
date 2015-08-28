fc6ea988195a411ffb8a7fc43ef9a36148a78d1d
Brandon Zuk <bzuk@premierlogic.com>
2015-08-28 13:46:10 +0000

    Changes made by Brandon Zuk (bzuk@premierlogic.com).

--- Checknet Application/js/checknet.angular.js
+++ Checknet Application/js/checknet.angular.js
@@ -1,50 +1,9 @@
-/* var myApp = angular.module('checkApp', ['kendo.directives']);
-          $(window).load(function () {
-              var app = new kendo.mobile.Application($(document.body), {
-                  skin: "nova"
-              });
-          });
 
-*/
-
-
-/*angular.module('checkApp', ['kendo.directives'])*/
-
-/*var myApp = angular.module('checkApp', ['kendo.directives']);
-         $(window).load(function () {
-             var app = new kendo.mobile.Application($(document.body), {
-                 skin: "nova",
-             });
-         });*/
-
-/*var app = new kendo.mobile.Application($(document.body), { skin: 'nova' });*/
 
 angular.module('checkApp', ['kendo.directives'])
-    .controller('test', function($scope) {
-        $scope.focusTextbox = function() {
-          console.log(1);
-            $("#input").focus().select();
-         
-        }
-    })
+    .controller('index', function($scope) {
+ })
 
-angular.module('directivesModule').directive('focusOnClear', [
-    '$timeout',
-    function (timeout) {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                var id = attrs.focusOnClear;
-                var $inputSearchElement = $(element).parent().find('#' + id);
-                element.on('click', function (event) {
-                    event.preventDefault();
-                    timeout(function () {
-                        $inputSearchElement.focus();
-                    }, 500);
-                });
-            }
-        };
-    }
-]);
+
 
 
